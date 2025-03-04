c911b713eeb04f627023b901ef46a8f57a5c45c0
neiger <neiger.serrano.wks@gmail.com>
2025-03-04 14:53:31 -0600

    Initial Commit

---
+++ qatrainingapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  qatrainingapp
+//
+//  Created by Neiger Serrano on 4/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ qatrainingappTests/qatrainingappTests.swift
@@ -0,0 +1,17 @@
+//
+//  qatrainingappTests.swift
+//  qatrainingappTests
+//
+//  Created by Neiger Serrano on 4/3/25.
+//
+
+import Testing
+@testable import qatrainingapp
+
+struct qatrainingappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
