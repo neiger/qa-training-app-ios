29492763cfcc7e38f0c0456355e4d04e41d58728
neiger <neiger.serrano.wks@gmail.com>
2025-05-16 10:37:02 -0600

    minor changes

--- qatrainingappTests/qatrainingappTests.swift
+++ qatrainingappTests/qatrainingappTests.swift
@@ -4,14 +4,14 @@
 //
 //  Created by Neiger Serrano on 4/3/25.
 //
-
-import Testing
-@testable import qatrainingapp
-
-struct qatrainingappTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
-}
+//
+//import Testing
+////@testable import qatrainingapp
+//
+//struct qatrainingappTests {
+//
+//    @Test func example() async throws {
+//        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+//    }
+//
+//}
