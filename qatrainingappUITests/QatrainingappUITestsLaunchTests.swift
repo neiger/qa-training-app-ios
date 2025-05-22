ce2a0c5f796406460fb9b063a55b05d349a290c2
neiger <neiger.serrano.wks@gmail.com>
2025-05-22 10:34:32 -0600

    Fix code validations

--- qatrainingappUITests/QatrainingappUITestsLaunchTests.swift
+++ qatrainingappUITests/QatrainingappUITestsLaunchTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class qatrainingappUITestsLaunchTests: XCTestCase {
+final class QatrainingappUITestsLaunchTests: XCTestCase {
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }
